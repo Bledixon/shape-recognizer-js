228f7173487827e2916869dcfcba18ac3206acb4
A bad coder <12498599+Bledixon@users.noreply.github.com>
2023-05-15 22:41:46 +0200

    updating comments

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-// Shape Classifier (Training)
-// Coding Challenge
+// Initial Credits: 
 // The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/158-shape-classifier.html
 // https://youtu.be/3MqJzMvHE3E
-
-// Generate Dataset: https://github.com/CodingTrain/website/tree/gh-pages/CodingChallenges/CC_158_Shape_Classifier/dataset
-// Generate Dataset (port): https://editor.p5js.org/codingtrain/sketches/7leVIzy5l
-// Training: https://github.com/CodingTrain/website/tree/gh-pages/CodingChallenges/CC_158_Shape_Classifier/training
-// Mouse: https://editor.p5js.org/codingtrain/sketches/JgLVfCS4E
-// Webcam: https://editor.p5js.org/codingtrain/sketches/2hZGBkqqq
 
 let circles = [];
 let squares = [];
